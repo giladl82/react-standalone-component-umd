1f5638a7bde4b172713efc585cb6adfce47bb8d8
Gilad Lev-Ari <giladl82@gmail.com>
2018-02-01 23:49:46 +0200

    Adding method to component

--- fancy-umd-component/src/index.js
+++ fancy-umd-component/src/index.js
@@ -2,17 +2,33 @@
 import css from './style.css'
 
 export class Fancy extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      text: 'This is so Fancy!'
+    }
+
+    this.refreshData = this.refreshData.bind(this);
+  }
+
+  refreshData () {
+    this.setState({
+      text: 'Fancy and updated'
+    })
+  }
+
   render() {
     const style = {
       color: this.props.color ? this.props.color : ''
     }
 
     return (
-      <div style={style} className={css.fancy}>This is so Fancy!</div>
+      <div style={style} className={css.fancy}>{this.state.text}</div>
     );
   }
 }
 
 export const init = ((selector, props) => {
-  ReactDOM.render(<Fancy {...props} />, document.querySelector(selector));
+  return ReactDOM.render(<Fancy {...props} />, document.querySelector(selector));
 })
